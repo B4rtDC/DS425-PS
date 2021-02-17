1baded3ba1e6af4b1daec96eaa5b962870db9700
Bart De Clerck <declerck.bart@pm.me>
2021-02-17 09:36:25 +0100

    git settings

--- update.jl
+++ update.jl
@@ -2,9 +2,9 @@ using Pkg
 # change to course environment
 Pkg.activate(dirname(@__FILE__))
 
-
 using GitCommand
 # pull latest updates
 git() do git
+    run(`$git config pull.rebase false`)
     run(`$git pull`)
 end
