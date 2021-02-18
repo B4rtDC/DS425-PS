6e3ea27e4896793d7716bb2a28b0e6317b89ffa4
Bart De Clerck <declerck.bart@pm.me>
2021-02-18 16:03:24 +0100

    added projects

---
+++ Projects/Project 2 - machine learning.jl
@@ -0,0 +1,14 @@
+### A Pluto.jl notebook ###
+# v0.12.20
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 39cf2802-71fa-11eb-2ba9-aba632614050
+md"""
+# machine learning
+[project placeholder]
+"""
+
+# ╔═╡ Cell order:
+# ╟─39cf2802-71fa-11eb-2ba9-aba632614050
