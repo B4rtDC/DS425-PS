0f65a3d8152a379b4dc625ed13cb267b02e992bf
Bart De Clerck <declerck.bart@pm.me>
2021-02-25 15:02:13 +0100

    working!

--- PS/ps25.jl
+++ PS/ps25.jl
@@ -20,10 +20,10 @@ struct GProblem <: Problem
     goalstate::String
 end
 # update!!!
-struct GNode 
+struct GNode <: Node
     state::String
     parent::Union{GNode, Nothing}
-    action::Union{Nothing}
+    action::Union{Nothing, UnitRange}
     pathcost::Int
 end
 
@@ -44,19 +44,42 @@ function Ggoaltest(n::GNode, p::GProblem)
     return isequal(n.state, p.goalstate)
 end
 function Gsolution(n::GNode)
-    nodes = Vector{GNode}
+    nodes = Vector{GNode}()
     current = n
     while !isnothing(current.parent)
         push!(nodes, current)
         current = current.parent
     end
-    push!(nodes, current.parent)
+    push!(nodes, current)
+    for i in length(nodes):-1:1
+        @info nodes[i].state, nodes[i].action ,nodes[i].pathcost
+    end
     return nodes
 end
 
+function treesearch(p::T, r::N; goaltest::Function, 
+                                findactions::Function, 
+                                applyaction::Function, 
+                                solution::Function) where {T<:Problem, N<:Node}
+    fringe = DataStructures.Queue{N}()       # create FIFO Queue
+    enqueue!(fringe, r)                      # place root node in the Queue
+    while !isempty(fringe)
+        node = dequeue!(fringe)
+        for action in findactions(node)
+            child = N(applyaction(action, node) , node, action, node.pathcost + 1)
+            if child ∉ fringe
+                if goaltest(child, p)
+                    return solution(child)
+                end
+                enqueue!(fringe, child)
+            end
+        end
+    end
+    @warn "failed to find a solution"
+end;
+
+
 myproblem = GProblem("ABBC", "E")
 root_node = GNode(myproblem.initialstate, nothing, nothing, 0)
-@info myproblem, root_node
-#@warn typeof(root_node)(myproblem.initialstate, nothing, nothing, 0)
-@info Gfindactions(root_node)
-@info Gapplyaction(2:3,root_node)
+@info treesearch(myproblem, root_node, goaltest=Ggoaltest, 
+            findactions=Gfindactions, applyaction=Gapplyaction, solution=Gsolution)
